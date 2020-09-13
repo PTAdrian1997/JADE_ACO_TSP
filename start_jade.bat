8635ca5f76b2c805417ced61ea39e1617bee06a8
PTAdrian1997 <poptifreaadrian@gmail.com>
2020-09-13 11:38:18 +0300

    stop using the GUI

--- start_jade.bat
+++ start_jade.bat
@@ -1,1 +1,1 @@
-java -cp jade.jar;classfiles jade.Boot -gui -agents ant1:agents.AntAgent(10);ant2:agents.AntAgent(10);ant3:agents.AntAgent(10)
+java -cp jade.jar;classfiles jade.Boot -agents ant1:agents.AntAgent(10);ant2:agents.AntAgent(10);ant3:agents.AntAgent(10)
